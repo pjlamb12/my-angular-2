611a7743767ef444590fd7965a7c51bf9a6e624c
Preston Lamb <preston.j.lamb@gmail.com>
2015-09-09 23:55:04 -0600

    adding objects to a list

--- app/components/my-component/my-component.ts
+++ app/components/my-component/my-component.ts
@@ -10,8 +10,17 @@ import {Component, View, CORE_DIRECTIVES} from 'angular2/angular2';
 
 export class MyComponent{
 	counter : number = 0;
+	list : Array = [];
+	item : Object = {name: '', description: ''};
 
 	addOne() {
 		this.counter += 1;
+	}
+
+	addItem(name, description) {
+		this.list.push({name: name.value, description: description.value});
+		console.log(this.list);
+		name.value = '';
+		description.value = '';
 	}
 }
